923761e6dd1460f1f9aefff39c6532fcc436dc57
mgron <mgron@stud.ntnu.no>
2025-05-15 14:14:33 +0200

    Wind Dataanalyse

--- src/functions.py
+++ src/functions.py
@@ -1,6 +1,8 @@
 # import av alle bibliotekene vi trenger
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import missingno as msno
 from sklearn.preprocessing import LabelEncoder
 
 
@@ -19,29 +21,90 @@ def label_tidsforskyvning(dataset):
     print(dataset)
 
 
-# Finner det årlige gjennomsnittet for verdien til datasettet
-def average_year(dataset):
-    value = np.mean(dataset['verdi'])
-    unit = input("Hva er enheten til verdien i datasettet?")
-    print(f"Gjennomsnittlig verdi for datasettet er {value:.2f} {unit}")
-    return value
-
-
-# Regner ut gjennomsnittet for hver måned
-def average_month(dataset):
-
-    # Lager en ny kolonne i temperatur som forteller hvilken måned det er 
-    dataset['måned'] = dataset['referansetid'].dt.month
-
-    # Regner ut gjennomsnittet for hver måned
-    monthly_average = round(dataset.groupby('måned')['verdi'].mean(), 2)
-    print('gjennomsnittlig månedlig verdi er:')
-    print(monthly_average)
-    return(monthly_average)
-
-
 # Finner medianen
 def median(dataset):
     median = np.median(dataset['verdi'])
     print("Medianen er", median)
-    return(median)
+
+
+# Lager et enkelt stolpediagram over en serie med data + gjennomsnitt
+def average_month_bargraph(series, name, unit):
+    index = series.index
+    values = series.values
+    series_mean = [np.mean(series.values)]*len(series.index)
+
+    # Initiere subplots
+    fig, ax = plt.subplots()
+    # Plotte dataserie
+    data_bars = ax.bar(index, values)
+    # Plotte gjennomsnittet
+    mean_line = ax.plot(index, series_mean,
+                        label='Gjennomsnitt',
+                        linestyle='--',
+                        color='orange'
+                        )
+    # Formatering
+    legend = ax.legend(loc='upper center')
+    plt.title(name)
+    plt.xlabel('Måned')
+    plt.ylabel(unit)
+    ax.set_axisbelow(True)
+    plt.grid(axis='y')
+    plt.xticks(rotation=0, ticks=index, labels=index)
+
+    plt.show()
+
+# Finner det årlige gjennomsnittet for verdien til datasettet
+def average_year(dataset):
+    value = np.mean(dataset['verdi'])
+    print(f"Gjennomsnittlig verdi for datasettet er {value:.2f}")
+    return value
+
+
+# Regner ut gjennomsnitt gruppert etter de ulike verdiene i valgt kolonne
+def average_other(dataset):
+    column = input('Hvilken kolonne fra datasettet vil du gruppere med? Du kan også velge måned')
+
+    # Lager en ny kolonne i temperatur som forteller hvilken måned det er 
+    if column == 'måned':
+        dataset['måned'] = dataset['referansetid'].dt.month
+
+    # Regner ut gjennomsnittet for hver kolonne
+    other_average = round(dataset.groupby(column)['verdi'].mean(), 2)
+    return(other_average)
+
+
+# Finner standardavik 
+def std(dataset):
+    std = np.std(dataset['verdi'])
+    print('Standardavviket er', round(std, 2))
+    return float(std)
+
+
+# Finner øvre og nedre grense med hjelp av standardavvik
+def lower_upper_limit(dataset):
+    mean = average_year(dataset)
+    s = std(dataset)
+    threshold = 3
+    lower_limit = mean - threshold * s
+    upper_limit = mean + threshold * s
+
+    return(lower_limit, upper_limit)
+
+    print("Nedre grense:", lower_limit)
+    print("Øvre grense:", upper_limit)
+
+
+# Viser og teller eventuelle manglende verdier
+def missing_numbers(dataset):
+
+    # Teller om noen av verdiene er None eller NaN
+    count_nan = dataset.isnull().sum()
+    print(count_nan)
+
+    # Visualiserer hvor mange verdier hver kolonne har
+    msno.bar(dataset)
+
+    
+    data_missing = dataset[dataset['verdi'].isna()]
+    print(data_missing)
